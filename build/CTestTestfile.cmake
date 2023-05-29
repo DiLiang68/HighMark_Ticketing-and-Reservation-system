baa37c042e6f38d6786938b29cf3d8d53a0da10c
DiLiang68 <Di.Liang@student.uts.edu.au>
2023-05-29 15:26:40 +0800

    Init project.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/FCP/Extra2/HighMark_Ticketing-and-Reservation-system
-# Build directory: G:/FCP/Extra2/HighMark_Ticketing-and-Reservation-system/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
